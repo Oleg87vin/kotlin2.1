77c2414d42534d04f397932ee3bfa73a771d344e
Oleg <olegdubrovin@gmail.com>
2023-05-18 14:12:43 +0500

    task2.1edit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,22 @@
+import java.util.*
+
 fun main() {
 
     val tax: Double = 0.0075
-    val amount: Int = 10_000
+    var amount: Int = 0
     var sumTax = 0
 
+    println("Ведите сумму перевода")
+
+    val scan = Scanner(System.`in`)
+
+    amount = scan.nextLine().toInt()
 
     sumTax = ((amount * tax).toInt())
 
     if (sumTax < 35) sumTax = 35
 
     println(sumTax)
-
 }
 
 
